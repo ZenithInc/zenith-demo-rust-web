b4174b4ef6049f389cf91ad0121ece85fbd5cb4b
SuZenith <hello.hacking.icu@gmail.com>
2024-10-09 19:19:59 +0800

    feat(App): 引入 tracing 日志
    引入 tracing 日志库并初始化

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,39 @@ use axum::{
 };
 use axum::response::IntoResponse;
 use serde_json::json;
-
+use tracing::{event, Level};
+use tracing_subscriber::fmt::SubscriberBuilder;
 use rust_demo::routes::users::register_user_routes;
 use rust_demo::utils::config;
 
 #[tokio::main]
 async fn main() {
     config::init();
+
+    let log_path = std::env::var("LOG_PATH").unwrap_or_else(|_| "./logs".to_string());
+    let log_filename_prefix = std::env::var("LOG_FILEPATH_PREFIX")
+        .unwrap_or_else(|_| "log.json".to_string());
+
+    let file_appender = tracing_appender::rolling::daily(log_path, log_filename_prefix);
+
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    SubscriberBuilder::default()
+        .with_max_level(Level::INFO)
+        .with_writer(non_blocking)
+        .init();
+
+    event!(Level::INFO, "config initialized");
+
     let app = Router::new()
         .merge(register_user_routes())
         .layer(middleware::from_fn(error_handler));
 
     let bind = std::env::var("BIND")
         .unwrap_or_else(|_| "0.0.0.0:3000".to_string());
+
+    event!(Level::INFO, "server started at {}", bind);
+
     let listener = tokio::net::TcpListener::bind(bind).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
